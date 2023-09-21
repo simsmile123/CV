f1358b5cc0c3d87ad94fdf762d0231fe51fe3643
simsmile123 <76024949+simsmile123@users.noreply.github.com>
2023-09-20 20:20:37 -0400

    Add files via upload

---
+++ l081/CMakeFiles/l081.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/l081.dir/l081.cpp.o"
+  "CMakeFiles/l081.dir/l081.cpp.o.d"
+  "l081"
+  "l081.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/l081.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
